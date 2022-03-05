ab33fca5cb2b409a56120dcd9e4ea4357ddeefcb
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-05 14:34:58 +0100

    Update comments migration

--- database/migrations/2022_03_05_132955_create_comments_table.php
+++ database/migrations/2022_03_05_132955_create_comments_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('author');
+            $table->string('content');
             $table->timestamps();
         });
     }
